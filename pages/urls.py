16dc9b6824fd0ce4038ec2704777e99aea2e4db6
Shamsiddin-it <samsiddinarbobov@gmail.com>
2024-10-19 18:03:19 +0500

    two files

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import homePageView
+from .views import *
 
 urlpatterns = [
-    path("", homePageView, name="home"),
+    path("first/", homePageView, name="first"),
+    path("second/", homePageView2, name="second"),
+    path("third/", homePageView3, name="third"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -6,3 +6,11 @@ from django.template import loader
 def homePageView(request):
     template = loader.get_template("html.html")
     return HttpResponse(template.render())
+
+def homePageView2(request):
+    template = loader.get_template("html2.html")
+    return HttpResponse(template.render())
+
+def homePageView3(request):
+    template = loader.get_template("html3.html")
+    return HttpResponse(template.render())
